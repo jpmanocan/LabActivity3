e76f0df024372a06c14302fc44936882a1f714f1
jpmanocan <2015086515@ust-ics.mygbiz.com>
2018-09-08 13:22:21 +0800

    init commit

--- app/src/main/java/com/manocan/johnphillip/MainActivity.java
+++ app/src/main/java/com/manocan/johnphillip/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Styles and Material Design");
         setContentView(R.layout.activity_main);
     }
 }
